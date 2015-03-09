08f1723c5f97a6d8b7b31080900035b66bb667bf
marcelo.teixeira <mteixeira@rocket-internet.pt>
2015-03-09 21:05:17 +0000

    ADD more thinks profile and other bugs

--- app/views/clients/processes/index.blade.php
+++ app/views/clients/processes/index.blade.php
@@ -253,13 +253,15 @@
                             <i class="clip-file"></i> {{ trans('processes.attachments') }} <span class="caret"></span>
                         </a>
                         <ul role="menu" class="dropdown-menu pull-right">
-                            @foreach($p->attachs as $a)
-                            <li role="presentation">
-                                <a role="menuitem" tabindex="-1" href="{{ route('processes.downloadAttach', ['id' => $a->id]) }}">
-                                    <img src="{{ asset(Helper::getFileTypeImg(explode('.', $a->name)[1])) }}" width="16" height="16" /> {{ $a->name }}
-                                </a>
-                            </li>
-                            @endforeach
+                            <?php if(!empty($p->attachs)) : ?>
+                                @foreach($p->attachs as $a)
+                                <li role="presentation">
+                                    <a role="menuitem" tabindex="-1" href="{{ route('processes.downloadAttach', ['id' => $a->id]) }}">
+                                        <img src="{{ asset(Helper::getFileTypeImg(explode('.', $a->name)[1])) }}" width="16" height="16" /> {{ $a->name }}
+                                    </a>
+                                </li>
+                                @endforeach
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </td>
